de4485388ffe6e44d10b73c6917bf91abb3cdb9c
DylanAustin-TheDreamer <austindylan0@gmail.com>
2025-10-17 14:53:42 +0100

    Fix ALLOWED_HOSTS: add Heroku domain to resolve Bad Request 400

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'herokuapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', 'herokuapp.com', '127.0.0.1', 'const-collection-0f06bd9d4705.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
 # Application definition
